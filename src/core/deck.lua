48e388e390188ed5dfcf37b8782f287c9f064d6a
polebruh <60035559+polebruh@users.noreply.github.com>
2026-06-17 22:32:07 +0200

    Created the new hand class and fixed some OOP mistakes i made with the last push

--- src/core/deck.lua
+++ src/core/deck.lua
@@ -1,17 +1,22 @@
-local deck = {
-    cards = {},
-    discardPile = {}
-}
+local deck = {}
 
 function deck:new(o) --Intitialise an instance of the card class
     o = o or {} --Give a blank table if no object is given
     setmetatable(o,self)
     self.__index = self
+
+    o.cards = o.cards or {}
+    o.discardPile = o.discardPile or {}
+
     return o
 end
 
 function deck:addCard(card)
     table.insert(self.cards,card)
+end
+
+function deck:discard(card)
+    table.insert(self.discardPile,card)
 end
 
 function deck:draw() --return the card at the end of the cards array and remove it
@@ -20,7 +25,6 @@ function deck:draw() --return the card at the end of the cards array and remove 
     end
     local card = self.cards[#self.cards] --Access the index equal to the number of elements in the array. Lua starts at index 1 lmao
     table.remove(self.cards) --Apparently ommitting the index of the remove automatically takes away the last element
-    table.insert(self.discardPile,card) --TODO. Include a check for card types that wont go to the discard pile when played
     return card
 end
 
@@ -29,7 +33,6 @@ function deck:shuffleInDiscard() --Shuffle in discard pile
         print("Placeholder logic for losing the game?")
     end
     self.cards = self.discardPile --Swap the discard pile with the empty cards array
-    self.discardPile = {}
     self:shuffle() --Shuffle the deck
 end
 
@@ -38,8 +41,7 @@ function deck:shuffle()
     local size = #self.cards
     for i = 1,size,1 do --For every card in the deck
         local randomValue = math.random(1,#self.cards) --Get a random value in the array
-        local card = self.cards[randomValue] --get the entry at that value
-        table.remove(self.cards,randomValue) --remove it from the array so we dont encounter it again
+        local card = table.remove(self.cards,randomValue) --remove it from the array so we dont encounter it again and get the removed entry
         table.insert(shuffledCards,card) --add it to the new array
     end
     self.cards = shuffledCards --Update the cards array
