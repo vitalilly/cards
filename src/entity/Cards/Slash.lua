c2f0dabe7177646231383c345dc92b71106eff12
polebruh <60035559+polebruh@users.noreply.github.com>
2026-06-22 12:50:16 +0200

    Created an attack, defend and a modifier card. Should be used as a blueprint for all other cards

--- src/entity/Cards/Slash.lua
+++ src/entity/Cards/Slash.lua
@@ -7,9 +7,9 @@ function slash:init(o)
     o = o or {}
 
     self.targets = true
+    self.tag = self.tagList.Attack
 end
---target is the player this is played against
-function slash:play(target) --Deal 4 damage to target player
 
-    target.allSourcesOfDamage[self.player] = target.allSourcesOfDamage[self.player] + 4 --We use a key value pair using the player who played this card as the key and the amount of damage as the value.
+function slash:playTarget(target) --Deal 4 damage to target player
+    self:damage(4,target)
 end
